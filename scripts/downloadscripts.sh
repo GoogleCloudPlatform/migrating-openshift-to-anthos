36d062a9e855db4de8d2eb686409409c71a67586
Giovanni Galloro <ggalloro@users.noreply.github.com>
2021-07-21 15:13:06 +0200

    Update download script with new GCP github org url

--- scripts/downloadscripts.sh
+++ scripts/downloadscripts.sh
@@ -16,9 +16,9 @@
 
 mkdir scripts
 cd scripts
-wget https://raw.githubusercontent.com/VeerMuchandi/MigratingFromOpenShiftToGKE/main/scripts/migrateScript1.sh
-wget https://raw.githubusercontent.com/VeerMuchandi/MigratingFromOpenShiftToGKE/main/scripts/migrateScript2.sh
-wget https://raw.githubusercontent.com/VeerMuchandi/MigratingFromOpenShiftToGKE/main/scripts/exportApplicationManifests.sh
-wget https://raw.githubusercontent.com/VeerMuchandi/MigratingFromOpenShiftToGKE/main/scripts/exportSecrets.sh
+wget https://raw.githubusercontent.com/GoogleCloudPlatform/MigratingFromOpenShiftToGKE/main/scripts/migrateScript1.sh
+wget https://raw.githubusercontent.com/GoogleCloudPlatform/MigratingFromOpenShiftToGKE/main/scripts/migrateScript2.sh
+wget https://raw.githubusercontent.com/GoogleCloudPlatform/MigratingFromOpenShiftToGKE/main/scripts/exportApplicationManifests.sh
+wget https://raw.githubusercontent.com/GoogleCloudPlatform/MigratingFromOpenShiftToGKE/main/scripts/exportSecrets.sh
 chmod +x *.sh
 cd ..
